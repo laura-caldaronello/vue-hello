433b78240ded37fd077770020b5578af7889accc
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-18 14:05:04 +0100

    Esercizio base

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var root = new Vue({
+    el: '#root',
+    data: {
+        message: 'messaggio'
+    }
+});
+
+
+
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
