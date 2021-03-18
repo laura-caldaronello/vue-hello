a02451d60da49891b8de2c4d7a57fdff217d998b
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-18 15:15:03 +0100

    Con bonus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 // Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 var root = new Vue({
     el: '#root',
     data: {
-        message: 'messaggio'
+        message: {
+            inH1: 'Visualizzatore di immagini',
+            inH3: 'Inserire link all\'immagine',
+            inButton: 'Visualizza',
+            imgUrl: ''
+        },
+        myClasses: 'display-none'
+    },
+    methods: {
+        pickImgUrl() {
+            var myImg = document.getElementById('image');
+            var myInput = document.getElementById('npt');
+
+            this.myClasses = 'display-block beautify-img';
+            myImg.setAttribute('src',myInput.value);
+        }
     }
 });
-
-
-
-// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
